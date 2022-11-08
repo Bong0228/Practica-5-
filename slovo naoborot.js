87b7631039455557cced9cc5307c4112bb94924a
Андрей Каравцев <116781212+Bong0228@users.noreply.github.com>
2022-11-08 11:40:47 +0700

    Add files via upload

---
+++ slovo naoborot.js
@@ -0,0 +1,10 @@
+function reverseStr(str) {
+        var newStr = '', i;
+        for (i = str.length - 1; i >= 0; i--) {
+            newStr += str.charAt(i);
+        }
+        return newStr;
+    }
+
+    alert(reverseStr('Как жизнь?!'));
+    
